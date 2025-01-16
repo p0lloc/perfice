56a25a7a0d0a513e65409ea732a4c32386edf1e4
p0lloc <p0lloc@protonmail.com>
2025-01-16 11:13:07 +0100

    Setup basic testing with vitest

---
+++ tests/primitive.test.ts
@@ -0,0 +1,23 @@
+import {expect, test} from "vitest";
+import {comparePrimitives, pList, pString} from "../src/model/primitive";
+
+test("string primitives equal", () => {
+    expect(comparePrimitives(pString("test"), pString("test"))).toBeTruthy()
+})
+
+test("string primitives inequal", () => {
+    expect(comparePrimitives(pString("test"), pString("not the same"))).toBeFalsy()
+})
+
+test("list primitives equal", () => {
+    expect(comparePrimitives(pList([pString("test")]), pList([pString("test")]))).toBeTruthy()
+})
+
+test("list primitives inequal", () => {
+    expect(comparePrimitives(pList([pString("test")]), pList([pString("ok")]))).toBeFalsy()
+})
+
+test("list primitives wrong order inequal", () => {
+    expect(comparePrimitives(pList([pString("test"), pString("test2")]),
+        pList([pString("test2"), pString("test")]))).toBeFalsy()
+})
