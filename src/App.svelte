2b78fb29ba6d7cee123fe7ebe82bf30e2ae988fe
p0lloc <p0lloc@protonmail.com>
2025-01-23 08:02:37 +0100

    Add routing system, rename Trackables into TrackableView

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,16 @@
 <script lang="ts">
-    import Trackables from "@perfice/views/Trackables.svelte";
     import {appReady} from "./main";
+    import type {Route} from "@mateothegreat/svelte5-router";
+    import {Router} from "@mateothegreat/svelte5-router";
+    import TrackableView from "@perfice/views/TrackableView.svelte";
+    import FormEditorView from "@perfice/views/FormEditorView.svelte";
+
+    const routes: Route[] = [
+        {path: "/forms/(?<formId>.*)", component: FormEditorView},
+        {path: "/", component: TrackableView},
+    ]
 </script>
 
 {#if $appReady}
-    <Trackables/>
+    <Router {routes}/>
 {/if}
