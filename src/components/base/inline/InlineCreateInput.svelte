a2f2c321cc2b18791fd264b3baf05299d84061c8
p0lloc <p0lloc@protonmail.com>
2025-05-01 10:26:09 +0200

    Add proper create tag input

---
+++ src/components/base/inline/InlineCreateInput.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+    import {onMount} from "svelte";
+
+    let {onSubmit, class: className = 'w-24', onBlur}: {
+        onSubmit: (name: string) => void,
+        onBlur: () => void,
+        class?: string
+    } = $props();
+
+    let input: HTMLInputElement;
+
+    function onKeyDown(e: KeyboardEvent & { currentTarget: HTMLInputElement }) {
+        if (e.key !== "Enter") return;
+
+        onSubmit(e.currentTarget.value);
+        e.currentTarget.value = "";
+    }
+
+    onMount(() => {
+        input.focus();
+    });
+</script>
+
+<input type="text" bind:this={input} class="{className} rounded-xl text-sm"
+       placeholder="Name" onblur={onBlur} onkeydown={onKeyDown}/>
--- src/components/tag/TagCategoryContainer.svelte
+++ src/components/tag/TagCategoryContainer.svelte
@@ -1,13 +1,12 @@
 <script lang="ts">
     import {faCheck, faPen, faPlus, faTrash} from "@fortawesome/free-solid-svg-icons";
-    // noinspection ES6UnusedImports
-    import Fa from "svelte-fa";
     import {type CategoryList, UNCATEGORIZED_NAME} from "@perfice/util/category";
     import type {WeekStart} from "@perfice/model/variable/time/time";
     import type {Tag, TagCategory} from "@perfice/model/tag/tag";
     import TagValueCard from "@perfice/components/tag/TagValueCard.svelte";
     import IconButton from "@perfice/components/base/button/IconButton.svelte";
     import {tags} from "@perfice/stores";
+    import InlineCreateInput from "@perfice/components/base/inline/InlineCreateInput.svelte";
 
     let {category, date, weekStart, onTagClicked, onTagEdit, onDelete}: {
         category: CategoryList<TagCategory, Tag>,
@@ -19,13 +18,18 @@
     } = $props();
 
     let editing = $state(false);
+    let creatingTag = $state(false);
 
     function toggleEdit() {
         editing = !editing;
     }
 
-    async function createTag() {
-        await tags.createTag(prompt("Name") ?? "", category.category?.id ?? null);
+    function startCreateTag() {
+        creatingTag = true;
+    }
+
+    async function createTag(name: string) {
+        await tags.createTag(name, category.category?.id ?? null);
     }
 
     function onTagClick(tag: Tag, entryId: string | null) {
@@ -45,7 +49,7 @@
                 <IconButton class="text-xl" icon={faTrash} onClick={onDelete}/>
             {/if}
             <IconButton class="text-xl" icon={editing ? faCheck : faPen} onClick={toggleEdit}/>
-            <IconButton class="text-2xl" icon={faPlus} onClick={createTag}/>
+            <IconButton class="text-2xl" icon={faPlus} onClick={startCreateTag}/>
         </div>
     </h1>
     <hr>
@@ -54,5 +58,8 @@
             <TagValueCard {editing} tag={tag} onClick={(log) => onTagClick(tag, log)} date={date}
                           weekStart={weekStart}/>
         {/each}
+        {#if creatingTag}
+            <InlineCreateInput onBlur={() => creatingTag = false} onSubmit={(name) => createTag(name)}/>
+        {/if}
     </div>
 </div>
