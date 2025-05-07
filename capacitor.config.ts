576db0a5bc36a3b876ae8ec1f35c61ca208307a0
p0lloc <p0lloc@protonmail.com>
2025-05-07 10:27:35 +0200

    Change notification icon

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,6 +4,13 @@ const config: CapacitorConfig = {
     appId: 'io.perfice.app',
     appName: 'Perfice',
     webDir: 'dist',
+    plugins: {
+        LocalNotifications: {
+            smallIcon: "res://drawable/small_icon",
+            largeIcon: "res://drawable/splash",
+            iconColor: "#16A34A"
+        }
+    }
 };
 
 export default config;
