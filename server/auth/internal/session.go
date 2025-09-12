cf5762354d91d54b7b989aa1ddccd79ea7f21820
Matthew Macdonald-Wallace <matt@doics.co>
2025-09-12 17:05:08 +0100

    Add the missing JWT library so the auth container builds

--- server/auth/internal/session.go
+++ server/auth/internal/session.go
@@ -7,6 +7,7 @@ import (
 	"time"
 
 	"github.com/google/uuid"
+    "github.com/golang-jwt/jwt/v5"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"perfice.adoe.dev/mongoutil"
