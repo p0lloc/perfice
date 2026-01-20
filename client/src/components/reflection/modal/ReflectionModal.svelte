b0a4b25c56b990ca9a6c47fe119e06ed54635cde
p0lloc <p0lloc@protonmail.com>
2026-01-20 12:26:21 +0100

    Make it possible to customize log date in reflection modal.

--- client/src/components/reflection/modal/ReflectionModal.svelte
+++ client/src/components/reflection/modal/ReflectionModal.svelte
@@ -7,8 +7,6 @@
         type Reflection,
         type ReflectionWidgetAnswerState
     } from "@perfice/model/reflection/reflection";
-    // noinspection ES6UnusedImports
-    import Fa from "svelte-fa";
     import ReflectionPageButton from "@perfice/components/reflection/modal/ReflectionPageButton.svelte";
     import ReflectionPageRenderer from "@perfice/components/reflection/modal/ReflectionPageRenderer.svelte";
     import type {PrimitiveValue} from "@perfice/model/primitive/primitive";
@@ -16,12 +14,10 @@
     import FormEmbed from "@perfice/components/form/FormEmbed.svelte";
     import {ButtonColor} from "@perfice/model/ui/button";
     import Button from "@perfice/components/base/button/Button.svelte";
-    import DatePicker from "@perfice/components/base/datePicker/DatePicker.svelte";
     import {SimpleTimeScopeType} from "@perfice/model/variable/time/time";
-    import {faCalendar} from "@fortawesome/free-solid-svg-icons";
-    import {formatDateYYYYMMDDHHMMSS} from "@perfice/util/time/format";
     import {forms, reflections} from "@perfice/stores";
     import {getDefaultFormAnswers} from "@perfice/model/form/data";
+    import ObjectDatePicker from "@perfice/components/base/datePicker/ObjectDatePicker.svelte";
 
     let modal: Modal;
     let reflection = $state<Reflection>({} as Reflection);
@@ -37,7 +33,6 @@
     let nestedFormAnswers = $state<Record<string, PrimitiveValue>>({});
     let nestedFormCallback = $state<(answers: Record<string, PrimitiveValue>, timestamp: number) => void>(() => {
     });
-    let nestedFormTimeScope = $state(SimpleTimeScopeType.DAILY);
 
     let date = $state(new Date());
 
@@ -73,11 +68,9 @@
         let formById = await forms.getFormById(formId);
         if (formById == null) return;
 
-        date = new Date();
         nestedForm = formById;
         nestedFormAnswers = {...getDefaultFormAnswers(formById.questions), ...answers};
         nestedFormCallback = onLog;
-        nestedFormTimeScope = timeScope;
     }
 
     function logNestedForm() {
@@ -92,9 +85,14 @@
         nestedForm = null;
     }
 
+    function onDateChanged(newDate: Date) {
+        date = newDate;
+    }
+
     export function open(activeReflection: Reflection) {
         reflection = activeReflection;
         currentPageNumber = 0;
+        date = new Date();
         nestedForm = null;
         answerStates = Object.fromEntries(reflection.pages.flatMap(p => Object.entries(generateAnswerStates(p.widgets))));
         modal.open();
@@ -102,15 +100,17 @@
 </script>
 
 <Modal size={ModalSize.LARGE} type={ModalType.NONE} title={reflection.name} bind:this={modal} onEnter={next}>
+    {#snippet actions()}
+        <ObjectDatePicker
+                inputClasses="w-16 text-center"
+                value={date}
+                displayFormat="hh:ii"
+                onChange={onDateChanged}
+        />
+    {/snippet}
+
     {#if currentPage != null}
         {#if nestedForm != null}
-            {#if nestedFormTimeScope !== SimpleTimeScopeType.DAILY}
-                <div class="flex justify-end mb-4 gap-2 items-center text-gray-500 dark:text-white">
-                    <Fa icon={faCalendar}/>
-                    <DatePicker value={formatDateYYYYMMDDHHMMSS(date)} time={true}
-                                onChange={(v) => date = new Date(v)} disabled={false}/>
-                </div>
-            {/if}
             <div class="pb-20 md:pb-0">
                 <FormEmbed bind:this={nestedFormEmbed} questions={nestedForm.questions} answers={nestedFormAnswers}/>
             </div>
