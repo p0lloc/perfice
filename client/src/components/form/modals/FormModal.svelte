92948ca66d1027e46f67b741f542aa88b381daa8
p0lloc <p0lloc@protonmail.com>
2026-06-22 19:42:24 +0200

    Increase z-index for large form log button

--- client/src/components/form/modals/FormModal.svelte
+++ client/src/components/form/modals/FormModal.svelte
@@ -1,25 +1,28 @@
 <script lang="ts">
-    import {ModalType} from "@perfice/model/ui/modal.js";
+    import { ModalType } from "@perfice/model/ui/modal.js";
     import Modal from "@perfice/components/base/modal/Modal.svelte";
-    import type {Form, FormQuestion} from "@perfice/model/form/form";
+    import type { Form, FormQuestion } from "@perfice/model/form/form";
     import FormEmbed from "@perfice/components/form/FormEmbed.svelte";
-    import type {JournalEntry} from "@perfice/model/journal/journal";
-    import {type PrimitiveValue} from "@perfice/model/primitive/primitive";
+    import type { JournalEntry } from "@perfice/model/journal/journal";
+    import { type PrimitiveValue } from "@perfice/model/primitive/primitive";
     import FormTemplateButton from "@perfice/components/form/modals/FormTemplateButton.svelte";
-    import type {FormTemplate} from "@perfice/model/form/form.js";
-    import {extractAnswerValuesFromDisplay} from "@perfice/services/variable/types/list";
-    import type {TextOrDynamic} from "@perfice/model/variable/variable";
+    import type { FormTemplate } from "@perfice/model/form/form.js";
+    import { extractAnswerValuesFromDisplay } from "@perfice/services/variable/types/list";
+    import type { TextOrDynamic } from "@perfice/model/variable/variable";
     // noinspection ES6UnusedImports
     import Fa from "svelte-fa";
-    import {faCheck, faTrash} from "@fortawesome/free-solid-svg-icons";
+    import { faCheck, faTrash } from "@fortawesome/free-solid-svg-icons";
     import IconButton from "@perfice/components/base/button/IconButton.svelte";
-    import {forms, journal} from "@perfice/stores";
-    import {getDefaultFormAnswers} from "@perfice/model/form/data";
+    import { forms, journal } from "@perfice/stores";
+    import { getDefaultFormAnswers } from "@perfice/model/form/data";
     import ObjectDatePicker from "@perfice/components/base/datePicker/ObjectDatePicker.svelte";
 
-    let {largeLogButton = true, onDelete}: {
-        largeLogButton?: boolean,
-        onDelete?: (entry: JournalEntry) => void
+    let {
+        largeLogButton = true,
+        onDelete,
+    }: {
+        largeLogButton?: boolean;
+        onDelete?: (entry: JournalEntry) => void;
     } = $props();
 
     let form: Form = $state({} as Form);
@@ -38,9 +41,15 @@
     let modal: Modal;
     let embed: FormEmbed;
 
-    export function open(logForm: Form, formQuestions: FormQuestion[], displayFormat: TextOrDynamic[], logDate: Date,
-                         availableTemplates: FormTemplate[], existingAnswers?: Record<string, PrimitiveValue>, entry?: JournalEntry) {
-
+    export function open(
+        logForm: Form,
+        formQuestions: FormQuestion[],
+        displayFormat: TextOrDynamic[],
+        logDate: Date,
+        availableTemplates: FormTemplate[],
+        existingAnswers?: Record<string, PrimitiveValue>,
+        entry?: JournalEntry,
+    ) {
         form = logForm;
         date = logDate;
         format = displayFormat;
@@ -61,11 +70,18 @@
         modal.close();
     }
 
-    function createOrUpdateTemplate(name: string, answers: Record<string, PrimitiveValue>) {
+    function createOrUpdateTemplate(
+        name: string,
+        answers: Record<string, PrimitiveValue>,
+    ) {
         let extractedValues = extractAnswerValuesFromDisplay(answers);
 
         if (editingTemplate != null) {
-            forms.updateFormTemplate($state.snapshot(editingTemplate), name, extractedValues);
+            forms.updateFormTemplate(
+                $state.snapshot(editingTemplate),
+                name,
+                extractedValues,
+            );
         } else {
             forms.createFormTemplate(form.id, name, extractedValues);
         }
@@ -76,17 +92,23 @@
         if (answers == null) return; // If there are validation errors, don't save
 
         if (editEntry != null) {
-            journal.updateEntry({
-                ...editEntry,
-                answers,
-                timestamp: date.getTime()
-            }, format);
+            journal.updateEntry(
+                {
+                    ...editEntry,
+                    answers,
+                    timestamp: date.getTime(),
+                },
+                format,
+            );
         } else {
             journal.logEntry(form, answers, format, date.getTime());
         }
 
         if (currentTemplateName != null) {
-            createOrUpdateTemplate($state.snapshot(currentTemplateName), answers);
+            createOrUpdateTemplate(
+                $state.snapshot(currentTemplateName),
+                answers,
+            );
         }
 
         close();
@@ -97,7 +119,7 @@
     }
 
     function onTemplateSelected(template: FormTemplate) {
-        currentTemplateName = null
+        currentTemplateName = null;
         answers = template.answers;
         embed.invalidateValues();
     }
@@ -120,41 +142,60 @@
     }
 </script>
 
-<Modal type={ModalType.CONFIRM_CANCEL} title={form.name} bind:this={modal} onConfirm={confirm} onEnter={confirm}
-       leftTitle={true}>
+<Modal
+    type={ModalType.CONFIRM_CANCEL}
+    title={form.name}
+    bind:this={modal}
+    onConfirm={confirm}
+    onEnter={confirm}
+    leftTitle={true}
+>
     {#snippet actions()}
         <ObjectDatePicker
-                inputClasses="w-16 text-center"
-                value={date}
-                displayFormat="hh:ii"
-                onChange={onDateChanged}
+            inputClasses="w-16 text-center"
+            value={date}
+            displayFormat="hh:ii"
+            onChange={onDateChanged}
         />
 
         {#if editEntry != null}
-            <IconButton icon={faTrash} onClick={onDeleteClicked}/>
+            <IconButton icon={faTrash} onClick={onDeleteClicked} />
         {:else}
-            <FormTemplateButton {templates} onNew={onNewTemplate}
-                                onTemplateSelected={onTemplateSelected} onEditTemplate={onEditTemplate}/>
+            <FormTemplateButton
+                {templates}
+                onNew={onNewTemplate}
+                {onTemplateSelected}
+                {onEditTemplate}
+            />
         {/if}
     {/snippet}
 
     {#if currentTemplateName != null}
-        <input type="text" class="w-full border mt-2" bind:value={currentTemplateName} placeholder="Template name"/>
+        <input
+            type="text"
+            class="w-full border mt-2"
+            bind:value={currentTemplateName}
+            placeholder="Template name"
+        />
         {#if editingTemplate == null}
-            <p class="text-xs mt-2">A new template will be created with the provided answers.</p>
+            <p class="text-xs mt-2">
+                A new template will be created with the provided answers.
+            </p>
         {/if}
-        <hr class="my-4"/>
+        <hr class="my-4" />
     {/if}
 
     <div class="overflow-y-scroll scrollbar-hide md:pb-0 pb-20">
-        <FormEmbed bind:this={embed} questions={questions} answers={answers}/>
+        <FormEmbed bind:this={embed} {questions} {answers} />
     </div>
 
     {#if largeLogButton}
-        <button onclick={confirm} class="md:hidden fixed bottom-0 w-screen h-20 bg-green-500 flex-center
-    pointer-feedback:bg-green-600 text-white left-0">
-
-            <Fa icon={faCheck} size="2.0x"/>
+        <button
+            onclick={confirm}
+            class="md:hidden z-10 fixed bottom-0 w-screen h-20 bg-green-500 flex-center
+    pointer-feedback:bg-green-600 text-white left-0"
+        >
+            <Fa icon={faCheck} size="2.0x" />
         </button>
     {/if}
 </Modal>
